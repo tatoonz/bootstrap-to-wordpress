ecac2611674c7ad6436408116f30168d5f1d07bb
TaToonz <peerapath.t@gmail.com>
2018-11-19 12:11:33 +0700

    feat(home): make testimonials section dynamic

--- bootstrap2wordpress/page-home.php
+++ bootstrap2wordpress/page-home.php
@@ -84,6 +84,13 @@ $instructor_students = get_field('instructor_students');
 $instructor_reviews = get_field('instructor_reviews');
 $instructor_courses = get_field('instructor_courses');
 
+// testimonials section
+$testimonials = new WP_Query(array(
+  'post_type' => 'testimonial',
+  'orderby' => 'ID',
+  'order' => 'ASC'
+));
+
 get_header();
 ?>
 
@@ -304,54 +311,23 @@ get_header();
         <div class="col-sm-8 offset-sm-2">
           <h2>What People Are Saying About Brad</h2>
 
-          <div class="row testimonial">
-            <div class="col-sm-4">
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/brennan.jpg" alt="Brennan">
+          <?php while ($testimonials->have_posts()): $testimonials->the_post(); ?>
+            <div class="row testimonial">
+              <div class="col-sm-4">
+                <?php
+                  if (has_post_thumbnail()) {
+                    the_post_thumbnail();
+                  }
+                ?>
+              </div>
+              <div class="col-sm-8">
+                <blockquote>
+                  <?php the_content(); ?>
+                  <cite>&mdash; <?php the_title(); ?></cite>
+                </blockquote>
+              </div>
             </div>
-            <div class="col-sm-8">
-              <blockquote>
-                These videos are well created, concise, fast-paced, easy to follow, and just funny enough to keep you chuckling as you're slamming out lines of code. I've taken 3 courses from this instructor. Whenever I have questions he is right there with a simple solution or a helpful suggestion to keep me going forward with the course work.
-                <cite>&mdash; Brennan, graduate of all of Brad's courses</cite>
-              </blockquote>
-            </div>
-          </div>
-
-          <div class="row testimonial">
-            <div class="col-sm-4">
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ben.png" alt="Illustration of a man with a mustache">
-            </div>
-            <div class="col-sm-8">
-              <blockquote>
-                I found Brad to be a great teacher, and a very inspiring person. It's clear he is very passionate about helping designers learn to code, and I look forward to more courses from him!
-                <cite>&mdash; Ben, graduate of Build a Website from scratch with HTML</cite>
-              </blockquote>
-            </div>
-          </div>
-
-          <div class="row testimonial">
-            <div class="col-sm-4">
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/aj.png" alt="Illustration of a man with a beard">
-            </div>
-            <div class="col-sm-8">
-              <blockquote>
-                Brad is amazing and I honestly think he's the best tutor of all the courses I have taken on Udemy. Will defenitely be following him in the future. Thanks Brad!
-                <cite>&mdash; AJ, graduate of Code a Responsive Website with Bootstrap 3</cite>
-              </blockquote>
-            </div>
-          </div>
-
-          <div class="row testimonial">
-            <div class="col-sm-4">
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ernest.png" alt="Illustration of a man with a goatee">
-            </div>
-            <div class="col-sm-8">
-              <blockquote>
-                Brad is an excellent instructor. His content is super high quality, and you can see the love and care put into every section. The tutorials are the perfect length, and you feel lik you doing something right out the gate! I really can't believe this is free. I highly recommend taking advantage of this course.
-                <cite>&mdash; Ernest, graduate of Code Dynamic Websites with PHP</cite>
-              </blockquote>
-            </div>
-          </div>
-
+          <?php endwhile; ?>
         </div>
       </div>
     </div>
